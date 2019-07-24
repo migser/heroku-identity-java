42e63c4e7821bd638f864d6662d3bbb659f784ef
Miguel Serrano <mserranocaldeiro@mserrano-ltm3.internal.salesforce.com>
2019-07-24 15:28:56 +0200

    añadido expid

--- src/main/java/com/salesforce/saml/SAMLServlet.java
+++ src/main/java/com/salesforce/saml/SAMLServlet.java
@@ -44,7 +44,7 @@ import java.util.UUID;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 
-public class SAMLServlet extends HttpServlet{
+public class SAMLServlet extends HttpServlet {
 
     private static final String SAML_REQUEST = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" AssertionConsumerServiceURL=\"{0}\" Destination=\"{1}\" ID=\"_{2}\" IssueInstant=\"{3}\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" Version=\"2.0\"><saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">{4}</saml:Issuer></samlp:AuthnRequest>";
     private Boolean INITIALIZED = false;
@@ -55,8 +55,6 @@ public class SAMLServlet extends HttpServlet{
     @Override
     public void init() throws ServletException {
         String samlMetadata = System.getenv("SAML_METADATA");
-
-
 
         if (samlMetadata != null) {
 
@@ -74,7 +72,8 @@ public class SAMLServlet extends HttpServlet{
                     GetMethod method = new GetMethod(samlMetadata);
 
                     // Provide custom retry handler is necessary
-                    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
+                    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                            new DefaultHttpMethodRetryHandler(3, false));
 
                     // Execute the method.
                     int statusCode = client.executeMethod(method);
@@ -91,7 +90,7 @@ public class SAMLServlet extends HttpServlet{
 
                 } else {
 
-                    response = new String(Base64.decodeBase64(samlMetadata.getBytes("UTF-8")),"UTF-8");
+                    response = new String(Base64.decodeBase64(samlMetadata.getBytes("UTF-8")), "UTF-8");
 
                 }
 
@@ -105,7 +104,7 @@ public class SAMLServlet extends HttpServlet{
                 throw new ServletException("Error decoding SAML Metadata", e);
             }
 
-            //Setup XPath
+            // Setup XPath
             NamespaceContext namespaceContext = new SAMLNamespaceResolver();
             XPathFactory factory = XPathFactory.newInstance();
             XPath xpath = factory.newXPath();
@@ -115,21 +114,26 @@ public class SAMLServlet extends HttpServlet{
 
                 XPathExpression edXPath = xpath.compile("/md:EntityDescriptor");
                 NodeList edXPathResult = (NodeList) edXPath.evaluate(metadataDocument, XPathConstants.NODESET);
-                if (edXPathResult.getLength() != 1) throw new ServletException("No EntityDescriptor in SAML_METADATA");
+                if (edXPathResult.getLength() != 1)
+                    throw new ServletException("No EntityDescriptor in SAML_METADATA");
                 Node edNode = edXPathResult.item(0);
                 ISSUER = edNode.getAttributes().getNamedItem("entityID").getTextContent();
-                if (ISSUER == null)throw new ServletException("No entityID on Entity Descriptor in SAML_METADATA");
+                if (ISSUER == null)
+                    throw new ServletException("No entityID on Entity Descriptor in SAML_METADATA");
 
-                XPathExpression certXPath = xpath.compile("/md:EntityDescriptor/md:IDPSSODescriptor/md:KeyDescriptor/ds:KeyInfo/ds:X509Data/ds:X509Certificate");
+                XPathExpression certXPath = xpath.compile(
+                        "/md:EntityDescriptor/md:IDPSSODescriptor/md:KeyDescriptor/ds:KeyInfo/ds:X509Data/ds:X509Certificate");
                 NodeList certXPathResult = (NodeList) certXPath.evaluate(metadataDocument, XPathConstants.NODESET);
-                if (certXPathResult.getLength() != 1) throw new ServletException("No X509Certificate node");
+                if (certXPathResult.getLength() != 1)
+                    throw new ServletException("No X509Certificate node");
                 Node certNode = certXPathResult.item(0);
                 StringBuffer encodedCert = new StringBuffer("-----BEGIN CERTIFICATE-----\n");
                 encodedCert.append(certNode.getTextContent());
                 encodedCert.append("\n-----END CERTIFICATE-----\n");
                 String cert = encodedCert.toString();
-                if (cert == null)throw new ServletException("No cert in cert node");
-                //System.out.println(cert);
+                if (cert == null)
+                    throw new ServletException("No cert in cert node");
+                // System.out.println(cert);
 
                 try {
                     CertificateFactory cf = CertificateFactory.getInstance("X.509");
@@ -141,13 +145,16 @@ public class SAMLServlet extends HttpServlet{
                     throw new ServletException("Error getting PublicKey from Cert", e);
                 }
 
-                XPathExpression ssoXPath = xpath.compile("//md:SingleSignOnService[@Binding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect']");
+                XPathExpression ssoXPath = xpath.compile(
+                        "//md:SingleSignOnService[@Binding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect']");
                 NodeList ssoXPathResult = (NodeList) ssoXPath.evaluate(metadataDocument, XPathConstants.NODESET);
-                if (ssoXPathResult.getLength() != 1) throw new ServletException("No SingleSignOnService with Redirect Binding");
+                if (ssoXPathResult.getLength() != 1)
+                    throw new ServletException("No SingleSignOnService with Redirect Binding");
                 Node ssoNode = ssoXPathResult.item(0);
                 IDP_URL = ssoNode.getAttributes().getNamedItem("Location").getTextContent();
-                if (IDP_URL == null)throw new ServletException("No Location for SingleSignOnService with Redirect Binding");
-
+                if (IDP_URL == null)
+                    throw new ServletException("No Location for SingleSignOnService with Redirect Binding");
+                IDP_URL = IDP_URL + "/expid_estrella";
 
             } catch (XPathExpressionException e) {
                 throw new ServletException("Error Executing XPaths on Metadata", e);
@@ -163,11 +170,11 @@ public class SAMLServlet extends HttpServlet{
             System.out.println("SAML isn't yet initialized!");
         }
 
-
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         String logout = request.getParameter("logout");
         if (logout != null) {
@@ -178,9 +185,8 @@ public class SAMLServlet extends HttpServlet{
             return;
         }
 
-
         String url = request.getRequestURL().toString();
-        //herokuism
+        // herokuism
         url = url.replaceFirst("http", "https");
         String app = null;
         try {
@@ -188,8 +194,8 @@ public class SAMLServlet extends HttpServlet{
         } catch (URISyntaxException e) {
             throw new ServletException(e);
         }
-        if (!INITIALIZED)  {
-            //DO some error handling
+        if (!INITIALIZED) {
+            // DO some error handling
             request.setAttribute("URL", url);
             request.setAttribute("app", app);
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/configure.jsp");
@@ -198,7 +204,7 @@ public class SAMLServlet extends HttpServlet{
 
         } else {
 
-            //Pretty simply way to build a SAML Request.  Beats building a DOM...
+            // Pretty simply way to build a SAML Request. Beats building a DOM...
             String[] args = new String[5];
             args[0] = url;
             args[1] = IDP_URL;
@@ -210,23 +216,24 @@ public class SAMLServlet extends HttpServlet{
             String requestXml = html.format(args);
             byte[] input = requestXml.getBytes("UTF-8");
 
-            //Deflate that XML
+            // Deflate that XML
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             Deflater d = new Deflater(Deflater.DEFLATED, true);
             DeflaterOutputStream dout = new DeflaterOutputStream(baos, d);
             dout.write(input);
             dout.close();
 
-            //B64 encode it
+            // B64 encode it
             String encodedRequest = Base64.encodeBase64String(baos.toByteArray());
 
-            //URLEncode it
+            // URLEncode it
             String SAMLRequest = URLEncoder.encode(encodedRequest, "UTF-8");
 
-            //Redirect that browser
+            // Redirect that browser
             String relayState = request.getParameter("RelayState");
             String redirect = IDP_URL + "?SAMLRequest=" + SAMLRequest;
-            if (relayState != null) redirect += "&RelayState=" + relayState;
+            if (relayState != null)
+                redirect += "&RelayState=" + relayState;
             response.sendRedirect(redirect);
 
         }
@@ -234,24 +241,26 @@ public class SAMLServlet extends HttpServlet{
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         String url = request.getRequestURL().toString();
-        //herokuism
+        // herokuism
         url = url.replaceFirst("http", "https");
 
-        //Get the SAMLResponse and RelayState
+        // Get the SAMLResponse and RelayState
         String encodedResponse = request.getParameter("SAMLResponse");
         String relayState = request.getParameter("RelayState");
-        if ((relayState == null) || ( relayState.equals(""))) relayState = "/";
+        if ((relayState == null) || (relayState.equals("")))
+            relayState = "/";
 
-        //validate the response
+        // validate the response
         SAMLValidator sv = new SAMLValidator();
         try {
             Identity identity = sv.validate(encodedResponse, IDP_PUBLIC_KEY, null, ISSUER, url, url);
-            //DO something with the Identity
+            // DO something with the Identity
             JSONObject identityJSON = new JSONObject();
-            identityJSON.put("subject",identity.getSubject());
+            identityJSON.put("subject", identity.getSubject());
             Bag attributes = identity.getAttributes();
             Set keySet = attributes.keySet();
             Iterator iterator = keySet.iterator();
@@ -259,15 +268,14 @@ public class SAMLServlet extends HttpServlet{
                 String key = (String) iterator.next();
                 identityJSON.put(key, (ArrayList<String>) attributes.getValues(key));
             }
-            Cookie identityCookie = new Cookie("IDENTITY", Base64.encodeBase64URLSafeString(identityJSON.toString().getBytes("UTF-8")));
+            Cookie identityCookie = new Cookie("IDENTITY",
+                    Base64.encodeBase64URLSafeString(identityJSON.toString().getBytes("UTF-8")));
             response.addCookie(identityCookie);
         } catch (Exception e) {
             response.sendError(401, "Access Denied: " + e.getMessage());
             return;
         }
         response.sendRedirect(relayState);
-
-
 
     }
 
